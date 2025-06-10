ca2cb055e3e91b61e1cfa8dd7a6a478f5adf46e8
NavyaTejaNadella-tcore <Navya.Teja@TransCore.com>
2025-06-10 20:03:29 +0000

    Add Comments class with GenerateComment method for skill ratings

---
+++ code/SKILLS/Comments.cs
@@ -0,0 +1,21 @@
+// create a method that generates a comment for a given skill
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SKILLS
+{
+    public class Comments
+    {
+        public static string GenerateComment(string skillName, int skillLevel)
+        {
+            if (skillLevel < 1 || skillLevel > 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(skillLevel), "Skill level must be between 1 and 5.");
+            }
+
+            return $"The skill '{skillName}' is rated at level {skillLevel}.";
+        }
+    }
+}
